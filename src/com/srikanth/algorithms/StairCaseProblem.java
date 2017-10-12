cc651f27ac2ac27253e829aff1d9b1a8d3b607b1
Devarakonda, Srikanth - C <Devarakonda.Srikanth@principal.com>
2017-10-12 08:33:16 -0500

    StairCase Problem

---
+++ src/com/srikanth/algorithms/StairCaseProblem.java
@@ -0,0 +1,22 @@
+package com.srikanth.algorithms;
+
+public class StairCaseProblem {
+	// Function Definition
+	public static int findStep(int n){
+	    if(n==1 || n==0){
+	        return 1;
+	    }else if(n==2){
+	        return 2;
+	    }
+	    else{	        
+	        return findStep(n-1)+findStep(n-2)+findStep(n-3);
+	    }
+	}
+	
+	public static void main(String args[]){
+		for(int i=0;i<35;i++){
+			System.out.println("Number of ways you can climb "+i+" steps is--->"+findStep(i));
+		}
+	}
+	
+}
